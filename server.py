9995107de017d0dccfcb5137b07223d1c0156038
superbunny38 <superbunny38@gmail.com>
2025-05-28 19:06:45 -0700

    ch

--- server.py
+++ server.py
@@ -1,2 +1,8 @@
 import os
 from mcp.server.fastmcp import FastMCP
+
+instructions_str = f"This server answers information about Chaeeun Ryu and executees codes implemented by Chaeeun Ryu.\n"
+mcp = FastMCP(name = "MCP Server for Chaeeun Ryu", instructions=instructions_str)
+
+
+
